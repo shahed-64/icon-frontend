3235aef375fa2c8782057fbacf77904de5a2d50e
shahed-64 <shahedislam6475@gmail.com>
2026-09-19 23:16:03 +0600

    fix dot env file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tbap2u0robs"
+    "revision": "0.t605mhkct54"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
