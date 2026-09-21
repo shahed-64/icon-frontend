e225a9447a78734020c82805b73da744df39c243
shahed-64 <shahedislam6475@gmail.com>
2026-09-21 12:27:53 +0600

    fix others Payment

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9g44c7a8d38"
+    "revision": "0.62d4gk3kk0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
